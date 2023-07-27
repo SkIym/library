26beaf53c0642a25a76db588e8afa778172211e6
SkIym <abramjosh13@gmail.com>
2023-07-27 18:46:37 +0800

    Add status functionality

--- script.js
+++ script.js
@@ -6,13 +6,11 @@ const bodyOverlay = document.getElementById("cover-body");
 const cancelNewBook = document.getElementById("cancel-add-button");
 const newBookForm = document.getElementById("add-book-form");
 let removeBookBtns = [];
+let statusBtns = [];
 
 let newBookTitle = document.getElementById("book-title");
 let newBookAuthor = document.getElementById("book-author");
 let newBookPages = document.getElementById("book-pages");
-let newBookRead = document.getElementById("read-checkbox"); 
-
-let addingBook = false;
 
 const tableHeader = document.createElement("tr");
 const tableTitle = document.createElement("th");
@@ -42,6 +40,13 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
+// define changeStatus()
+Book.prototype.changeStatus = function() {
+  let btn = document.getElementById(`statusBtn${myLibrary.indexOf(this)}`)
+  let status = btn.textContent;
+  btn.textContent = (status === "Not Read") ? "Read" : "Not Read";
+}
+
 // Add Book to library array
 newBookForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -53,6 +58,8 @@ newBookForm.addEventListener("submit", (e) => {
 function addBookToLibrary(e) {
   e.preventDefault();
   const addedBook = new Book(newBookTitle.value, newBookAuthor.value, newBookPages.value);
+
+  // reset input fields
   newBookTitle.value = "";
   newBookAuthor.value = "";
   newBookPages.value = "";
@@ -67,6 +74,8 @@ function loadLibraryToTable() {
   tableDisplay.appendChild(tableHeader);
 
   myLibrary.forEach((book) => {
+
+    let bookIndex = myLibrary.indexOf(book);
     const bookRow = document.createElement("tr");
 
     const bookTitle = document.createElement("td");
@@ -78,35 +87,41 @@ function loadLibraryToTable() {
     const bookPages = document.createElement("td");
     bookPages.textContent = book.pages;
 
-    const blankCell = document.createElement("td");
-    blankCell.textContent = "";
+    const bookStatus = document.createElement("td");
+    const statusBtn = document.createElement("button");
+    statusBtn.classList.add("change-status-btn");
+    statusBtn.id = `statusBtn${bookIndex}`
+    statusBtn.value = `${bookIndex}`;
+    statusBtn.textContent = "Not Read";
+    bookStatus.appendChild(statusBtn);
 
     const removeBook = document.createElement("td");
     const removeBtn = document.createElement("button");
     removeBtn.classList.add("remove-book-btn");
-    removeBtn.value = `${myLibrary.indexOf(book)}`;
+    removeBtn.value = `${bookIndex}`;
     removeBtn.textContent = "Remove";
     removeBook.appendChild(removeBtn);
 
-    
 
     bookRow.appendChild(bookTitle);
     bookRow.appendChild(bookAuthor);
     bookRow.appendChild(bookPages);
-    bookRow.appendChild(blankCell);
+    bookRow.appendChild(bookStatus);
     bookRow.appendChild(removeBook);
     tableDisplay.appendChild(bookRow);
   });
-  updateRemoveButtons();
+  updateAuxButtons();
 }
 
 // Function to update new buttons
-function updateRemoveButtons() {
+function updateAuxButtons() {
   removeBookBtns = Array.from(document.querySelectorAll(".remove-book-btn"));
   removeBookBtns.forEach((btn) => {
-    console.log("hello?")
-    console.log(btn)
     btn.addEventListener("click", removeBookFromLibrary);
+  });
+  statusBtns = Array.from(document.querySelectorAll(".change-status-btn"));
+  statusBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {myLibrary[btn.value].changeStatus()});
   });
 }
 
@@ -122,7 +137,6 @@ function updateNewBook() {
 function removeBookFromLibrary(e) {
   let bookIndex = e.target.value;
   myLibrary.splice(bookIndex, 1);
-  console.log(myLibrary)
   loadLibraryToTable();
 }
 
